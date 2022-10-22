54a7f84c97eef6f4010e2c2620a993bce317870e
Priyanshu Anand <96513495+sparrowpri@users.noreply.github.com>
2022-10-22 15:14:57 +0530

    Time: 130 ms (67.72%), Space: 37.8 MB (46.06%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int SumOfLeftLeaves(TreeNode root) {
+        if(root==null)
+        {
+            return 0;
+        }
+        int ans=0;
+        if(root.left!=null)
+        {
+            if(root.left.left==null && root.left.right==null)
+            {
+                ans+=root.left.val;
+                
+            }
+        }
+        ans+=SumOfLeftLeaves(root.left)+SumOfLeftLeaves(root.right);
+        
+        return ans;
+    }
+}
